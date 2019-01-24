b63a4f67849d09d702c446aa20ccb80156500be1
Kristen Kulha <kristen.m.kulha@gmail.com>
2019-01-24 11:33:30 -0800

    Restyle contact component and add media queries, change footer font size.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import '../src/scss/styles.scss';
 import '@fortawesome/fontawesome-free/js/all';
+import $ from 'jquery';
+
+$('#j-nav').on('click','a', function(event){ 
+  event.preventDefault();
+  let dest = $(this).attr("href");
+  if (dest === '#') {
+    window.scrollTo(0,0);
+  } else {
+    let elementOffset =  $(dest).offset();   
+    let navHeight = elementOffset.top - 65;
+    window.scrollTo(0, navHeight);
+  }   
+});
+
+if (navigator.userAgent.match(/OS X.*Safari/) && ! navigator.userAgent.match(/Chrome/)) {
+    document.getElementById('j-banner-header').className += 'safari';
+}
