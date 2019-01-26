09cc9df32cd1ff31e973921eab97620356ba3837
Kristen Kulha <kristen.m.kulha@gmail.com>
2019-01-26 09:10:05 -0800

    Add CNAME file.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: "kristenkulha.com",
   },
 
   devtool: 'eval-source-map',
